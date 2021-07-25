6ea4712d3f682b2869541f040168d44e3304b40e
hemangandhi <hemangandhi@gmail.com>
2021-07-24 23:27:39 -0400

    Nix set up and truncation of some API methods we won't need

---
+++ dev.nix
@@ -0,0 +1,14 @@
+# Nix packages version of "the set of things you need in your system to code
+# this"
+
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "sukuraberu-dev-env";
+  buildInputs = [
+    # Python
+    python36
+    gcc
+    libffi
+  ];
+}
